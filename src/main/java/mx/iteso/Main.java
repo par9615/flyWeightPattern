64a909aa9040f9668d7d734b612c8778418d494a
Miguel Hernández <par9615@gmail.com>
2016-11-13 18:57:44 -0600

    Classes added

--- src/main/java/mx/iteso/Main.java
+++ src/main/java/mx/iteso/Main.java
@@ -4,4 +4,18 @@ package mx.iteso;
  * Created by simio on 09/11/2016.
  */
 public class Main {
+
+    public static void main(String[] args){
+
+        Circle circle1 = (Circle)ShapeFactory.getCircle("Red");
+
+
+        circle1.setX(10);
+        circle1.setY(20);
+        circle1.draw();
+
+        Circle circle2 = (Circle) ShapeFactory.getCircle("Red");
+        circle2.draw();
+
+    }
 }
---
+++ src/main/java/mx/iteso/Shape.java
@@ -0,0 +1,8 @@
+package mx.iteso;
+
+/**
+ * Created by simio on 13/11/2016.
+ */
+public interface Shape {
+    public void draw();
+}
